88af2e04b102b52dd77c2398ca64c69578ed72fe
csenf <carsten.senf@googlemail.com>
2014-10-10 09:43:03 +0200

    Filetype 'handlebars' treated as 'html'.

--- main.js
+++ main.js
@@ -208,6 +208,7 @@ define(function (require, exports, module) {
         case 'php':
         case 'xml':
         case 'ejs':
+        case 'handlebars':
             formattedText = _formatHTML(unformattedText, indentChar, indentSize);
             batchUpdate(formattedText, isSelection);
             break;
