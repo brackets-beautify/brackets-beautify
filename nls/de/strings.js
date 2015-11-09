7856c2bda61acf5a4182d02ded32f9650b00c3ce
Sebastian Herrmann <sebastian@herrherrmann.net>
2015-11-09 19:02:24 +0100

    Update German translation strings and package.json.

--- nls/de/strings.js
+++ nls/de/strings.js
@@ -1,11 +1,12 @@
 define({
+    BEAUTIFY: 'Beautify',
     BEAUTIFY_ON_SAVE: 'Beautify beim Speichern',
     BEAUTIFY_ON_SAVE_DESC: 'Aktiviert Beautify beim Speichern',
     UNSUPPORTED_TITLE: 'Sprache nicht unterstützt',
-    UNSUPPORTED_MESSAGE: 'Diese Sprache ist nicht unterstützt.\n Verfügbare Sprachen sind JavaScript, JSON, HTML, XML, SVG, HTML in PHP, Embedded JavaScript, Handlebars, CSS, SCSS, und LESS.',
+    UNSUPPORTED_MESSAGE: 'Diese Sprache wird nicht unterstützt.\n Verfügbare Sprachen sind JavaScript, JSON, HTML, XML, SVG, HTML in PHP, Embedded JavaScript, Handlebars, CSS, SCSS und LESS.',
 
     SASS_PREF_NAME: 'Pfad zum sass-convert Programm',
     SASS_PREF_DESC: 'Absoluter Pfad zum sass-convert Programm',
-    SASS_FORMAT: 'Fehler beim formatieren der SASS Datei',
+    SASS_FORMAT: 'Fehler beim Formatieren der SASS-Datei',
     SASS_ERROR: 'Bitte gib einen Pfad zum SASS-Convert Programm an.'
 });
