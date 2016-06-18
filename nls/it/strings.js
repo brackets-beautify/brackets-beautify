a25e7f95149acac3b277eac8f62c903f5bf3197d
Denis R <student@alice.it>
2016-06-18 20:49:37 +0200

    Update strings.js

--- nls/it/strings.js
+++ nls/it/strings.js
@@ -1,7 +1,17 @@
 define({
     BEAUTIFY_ON_SAVE: 'Beautify al salvataggio',
     UNSUPPORTED_TITLE: 'Linguaggio non supportato',
-    UNSUPPORTED_MESSAGE: 'Questo linguaggio non é supportato.<br />I linguaggi supportati sono: JavaScript, JSON, HTML, XML, SVG, HTML in PHP, Embedded JavaScript, Handlebars, CSS, SCSS e LESS.'
+    UNSUPPORTED_MESSAGE: 'Questo linguaggio non é supportato.<br />I linguaggi supportati sono: JavaScript, JSON, HTML, XML, SVG, HTML in PHP, Embedded JavaScript, Handlebars, CSS, SCSS e LESS.',
+    BEAUTIFY: 'Beautify',
+    BEAUTIFY_ON_SAVE_DESC: 'true Beautify salva il file al salvataggio',
+    PREF_BEAUTIFIERS_NAME: 'Esterno Beautifiers',
+    PREF_BEAUTIFIERS_DESC: 'Esterno Beautifiers, vedi https://github.com/brackets-beautify/brackets-beautify#external-formatters',
+    PREF_LANGUAGES_NAME: 'Beautifier-Language-Mapping',
+    PREF_LANGUAGES_DESC: 'Beautifier-Language-Mapping, vedi https://github.com/brackets-beautify/brackets-beautify#configure-languages',
+    PREF_DIALOG_NAME: 'Nascondi finestra di avvertimento per i linguaggi non supportate',
+    PREF_DIALOG_DESC: 'true per nascondere una finestra di avvertimento per i linguaggi non supportate',
+    CONFIGURE_LINKS: 'Vedi il README per <a href="https://github.com/brackets-beautify/brackets-beautify#configure-languages">configurazione linguaggi</a> e <a href="https://github.com/brackets-beautify/brackets-beautify#external-formatters">aggiunte di nuovi formati</a>.',
+    UNSUPPORTED_CHECKBOX: 'Non mostrare questo dialogo in futuro'
 });
 
-/* Last translated for v.1.0.2 (a4466ce026f101107a3fab4fc371194a47868473) */
+/* Last translated for v.2.2.0 */
