3f0ea1aff595afce36ab044bcbeb567018368625
Michał Korczak <michal.k2001@gmail.com>
2017-07-02 15:48:36 +0200

    Polish translation

---
+++ nls/pl/strings.js
@@ -0,0 +1,17 @@
+define({
+    BEAUTIFY: 'Upiększ',
+    BEAUTIFY_ON_SAVE: 'Upiększ przy zapisie',
+    BEAUTIFY_ON_SAVE_DESC: 'tak aby Upiększać przy Zapisie',
+
+    PREF_BEAUTIFIERS_NAME: 'Zewnętrzne Upiększacze',
+    PREF_BEAUTIFIERS_DESC: 'Zewnętrzne Upiększacze, zobacz https://github.com/brackets-beautify/brackets-beautify#external-formatters',
+    PREF_LANGUAGES_NAME: 'Mapowanie Języka Upiększania',
+    PREF_LANGUAGES_DESC: 'Mapowanie Języka Upiększania, zobacz https://github.com/brackets-beautify/brackets-beautify#configure-languages',
+
+    PREF_DIALOG_NAME: 'Ukryj dialog dla niewspieranych języków',
+    PREF_DIALOG_DESC: 'tak aby ukryć dialog ostrzegający o niewspieranych językach',
+    UNSUPPORTED_TITLE: 'Niewspierany język',
+    UNSUPPORTED_MESSAGE: 'Ten język nie jest wspierany.<br />Wspieranymi językami są JavaScript, JSON, HTML, XML, SVG, HTML w PHP, osadzony JavaScript, Handlebars, CSS, SCSS, i LESS.',
+    CONFIGURE_LINKS: 'Zobacz PRZECZYTAJ dla <a href="https://github.com/brackets-beautify/brackets-beautify#configure-languages">konfigurowania języków</a> i <a href="https://github.com/brackets-beautify/brackets-beautify#external-formatters">dodawania nowych formaterów</a>.',
+    UNSUPPORTED_CHECKBOX: 'Nie pokazuj tego komunikatu w przyszłości'
+});
