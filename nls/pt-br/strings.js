1731bbb3946b3783e2901f8a2d8cc7912f6fe663
Artur Guedes <arturguedx@gmail.com>
2019-03-30 23:41:20 -0300

    Brazilian Portuguese translation (#282)
    * Brazilian Portuguese translation
    
    * Brazilian Portuguese translation

---
+++ nls/pt-br/strings.js
@@ -0,0 +1,17 @@
+define({
+    BEAUTIFY: 'Beautify',
+    BEAUTIFY_ON_SAVE: 'Beautify ao salvar',
+    BEAUTIFY_ON_SAVE_DESC: 'true para Beautify arquivo ao salvar',
+
+    PREF_BEAUTIFIERS_NAME: 'Beautifiers Externos',
+    PREF_BEAUTIFIERS_DESC: 'Beautifiers Externos, acesse https://github.com/brackets-beautify/brackets-beautify#external-formatters',
+    PREF_LANGUAGES_NAME: 'Beautifier-Language-Mapping',
+    PREF_LANGUAGES_DESC: 'Beautifier-Language-Mapping, acesse https://github.com/brackets-beautify/brackets-beautify#configure-languages',
+
+    PREF_DIALOG_NAME: 'Ocultar dialogo para linguagens não suportadas',
+    PREF_DIALOG_DESC: 'true para ocultar dialogo de linguagens não suportadas',
+    UNSUPPORTED_TITLE: 'Linguagem não suportada',
+    UNSUPPORTED_MESSAGE: 'Esta linguagem não é suportada pelo Beautify.<br /> As linguagens suportadas são: JavaScript, JSON, HTML, XML, SVG, HTML in PHP, Embedded JavaScript, Handlebars, CSS, SCSS e LESS.',
+    CONFIGURE_LINKS: 'Acesse o README para <a href="https://github.com/brackets-beautify/brackets-beautify#configure-languages">configurar linguagens</a> e <a href="https://github.com/brackets-beautify/brackets-beautify#external-formatters">usar formatadores externos</a>.',
+    UNSUPPORTED_CHECKBOX: 'Não exibir essa mensagem no futuro'
+});
